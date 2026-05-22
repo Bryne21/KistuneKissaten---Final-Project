38e41f3dc24515a85e7c790ebce2f05926fb6a6f
Bryne21 <nedacosta21@gmail.com>
2026-05-22 10:59:22 +0800

    fix na po sana

--- server/index.js
+++ server/index.js
@@ -29,6 +29,10 @@ const Menu = mongoose.model("Menu", menuSchema);
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
 // dc connection
 mongoose
